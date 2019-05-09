f9ad890757618da7a8d1fe34c9c8081fbd86a7be
caiguangzu <caiguangzu@cjdgy.com>
2019-05-09 11:43:49 +0800

    first commit

---
+++ app/src/main/java/com/blood/dbframe/MainActivity.java
@@ -0,0 +1,13 @@
+package com.blood.dbframe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
